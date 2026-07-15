aab8aba02521579c869140f4a43076bc0a43151e
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-07-15 17:10:31 +0600

    Initial commit

--- src/main/java/com/appsdeveloperblog/tutorials/spring/mvc/estore/UsersController.java
+++ src/main/java/com/appsdeveloperblog/tutorials/spring/mvc/estore/UsersController.java
@@ -3,14 +3,24 @@ package com.appsdeveloperblog.tutorials.spring.mvc.estore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsersController {
 
-    @GetMapping(path="/users/{userId}")
-    public ModelAndView getUser(@PathVariable("userId") String userId) {
-        ModelAndView modelAndView = new ModelAndView("user");
+    @GetMapping(path="/users/{userId}/albums/{albumId}")
+    public ModelAndView getAlbum(@PathVariable("userId") String userId,
+                                @PathVariable("albumId") String albumId) {
+        ModelAndView modelAndView = new ModelAndView("album");
+        modelAndView.addObject("userId", userId);
+        modelAndView.addObject("albumId", albumId);
+        return modelAndView;
+    }
+
+    @GetMapping(path="/users")
+    public ModelAndView getUsers(@RequestParam(name="limit", required = false) int limit) {
+        ModelAndView modelAndView = new ModelAndView("users");
         return modelAndView;
     }
 }
