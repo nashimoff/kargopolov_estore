fd2ddb00de487280e1677e0048c060fb6257708c
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-07-15 15:12:54 +0600

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/tutorials/spring/mvc/estore/EstoreApplication.java
@@ -0,0 +1,15 @@
+package com.appsdeveloperblog.tutorials.spring.mvc.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EstoreApplication.class, args);
+	}
+
+}
+//nurlan
